bd3f4bda9e0c69404fddbf154aa06f9ef54209e4
serkan <shaytac@gmail.com>
2019-09-12 17:31:33 -0400

    comment added to v5

--- hello-world/v5/hello_test.go
+++ hello-world/v5/hello_test.go
@@ -5,6 +5,8 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t *testing.T, got, want string) {
+		/* t.Helper() is needed to tell the test suite that this method is a helper.
+		By doing this when it fails the line number reported will be in our function call rather than inside our test helper */
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -13,7 +15,7 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Chris")
-		want := "Hello, Chris"
+		want := "Hello, Chri"
 		assertCorrectMessage(t, got, want)
 	})
 
